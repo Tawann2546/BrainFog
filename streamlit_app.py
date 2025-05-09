ccf1894857a9f5597089b050b80e5561d60e81a9
narabadi saeli <113263515+Tawann2546@users.noreply.github.com>
2025-05-10 01:01:09 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import train_test_split, GridSearchCV
 
 
